b73875df523e58e88b8f20dc231e637bd379ec24
itsJRillo <jmorillolabour@gmail.com>
2023-04-20 13:08:19 +0200

    Various Changes

--- Assets/Scripts/Gameplay/CountdownTimer.cs
+++ Assets/Scripts/Gameplay/CountdownTimer.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 using TMPro;
 
 public class CountdownTimer : MonoBehaviour
 {
-    public float timeRemaining = 120.0f;
+    public float timeRemaining = 5.0f;
     public static bool gameOver = false;
     public TMP_Text timerText;
 
     void Update() {
-        timeRemaining -= Time.deltaTime;
         if (timeRemaining <= 0.0f || gameOver == true) {
             timerText.text = "Time's up";
+            Time.timeScale = 0f;
+        } else {
+            timeRemaining -= Time.deltaTime;
+
+            int minutes = Mathf.FloorToInt(timeRemaining / 60.0f);
+            int seconds = Mathf.FloorToInt(timeRemaining % 60.0f);
+        
+            timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
         }
-        int minutes = Mathf.FloorToInt(timeRemaining / 60.0f);
-        int seconds = Mathf.FloorToInt(timeRemaining % 60.0f);
-    
-        timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
     }
 
 
